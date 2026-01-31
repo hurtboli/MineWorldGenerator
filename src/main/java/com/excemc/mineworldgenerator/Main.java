5ddbed4000d9d716cd180d65063125387d46b94c
hurtboli <105544072+hurtboli@users.noreply.github.com>
2026-01-31 17:01:27 +0800

    Update Main.java
    主类精简

--- src/main/java/com/excemc/mineworldgenerator/Main.java
+++ src/main/java/com/excemc/mineworldgenerator/Main.java
@@ -6,14 +6,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
     private ConfigManager configManager;
 
-    @Override
     public void onEnable() {
-        configManager = new ConfigManager(this);
-        getLogger().info("矿场世界生成器已加载");
+        this.configManager = new ConfigManager(this);
+        this.getLogger().info("矿场世界生成器已加载");
     }
 
-    @Override
     public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
-        return new WorldGenerator(configManager);
+        return new WorldGenerator(this.configManager);
     }
 }
